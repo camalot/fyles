77c0b570cb88a9d70b6de4a26c13047bb7254364
Ryan Conrad <camalot@gmail.com>
2013-08-11 10:45:32 -0500

    added a max size - default: 48px
    updated the sample.
    usage: fyles.exe /max=32

--- Fyles/Program.cs
+++ Fyles/Program.cs
@@ -15,82 +15,50 @@ namespace Fyles {
 	class Program {
 		private const string FYLES = "fyles";
 		private const String DEFAULT = "___DEFAULT___";
+
+		// the widths of the icons
+		private static readonly List<int> Widths = new List<int> ( );
+
+		// save the hashes of each icon
+		private static readonly Dictionary<String, String> Hashes = new Dictionary<String, String> ( );
+		// a place to track the dupes so we can delete them.
+		private static readonly Dictionary<String, List<String>> Dupes = new Dictionary<String, List<String>> ( );
+
+		private static DirectoryInfo OutputDirectory;
+		private static int MaxSize = 48;
 		static void Main ( string[] args ) {
+			var arguments = new Arguments ( args );
+			MaxSize = arguments.ContainsKey ( "max", "m" ) ? int.Parse ( arguments["max", "m"] ) : 48;
 			var path = Path.Combine ( Path.GetDirectoryName ( Assembly.GetAssembly ( typeof ( Program ) ).Location ), "Output" );
-			var directory = new DirectoryInfo ( path );
-			if ( !directory.Exists ) {
-				directory.Create ( );
+			OutputDirectory = new DirectoryInfo ( path );
+			if ( OutputDirectory.Exists ) {
+				OutputDirectory.Delete(true);
 			}
+			OutputDirectory.Create();
 			// all the icons
 			var icons = IconReader.GetFileTypeAndIcon ( );
 
 			// add special UNKNOWN/DEFAULT
 			icons.Add ( ".{0}".With ( DEFAULT ), "" );
 
-			// the widths of the icons
-			var widths = new List<int> ( );
-
-			// save the hashes of each icon
-			var hashes = new Dictionary<String, String> ( );
-			// a place to track the dupes so we can delete them.
-			var dupes = new Dictionary<String, List<String>> ( );
 
 			foreach ( var icon in icons ) {
-				foreach ( var esize in Enum.GetValues ( typeof ( IconReader.IconSize ) ) ) {
-					var shfi = new Fyles.WinApi.Shell32.SHFILEINFO ( );
-
-					using ( var ico = IconReader.ExtractIconFromFileEx ( FYLES + icon.Key, (IconReader.IconSize)esize, ref shfi ) ) {
-						if ( ico == null ) {
-							continue;
-						}
-
-						var size = String.Format ( "{0}", ico.Height, ico.Width );
-						if ( !widths.Contains ( ico.Width ) ) {
-							widths.Add ( ico.Width );
-						}
-						using ( var img = ico.ToBitmap ( ) ) {
-							var iconKey = icon.Key.Substring ( 1 );
-							var iks = String.Format ( "{0}-{1}.png", iconKey, size );
-							if ( hashes.ContainsKey ( iks ) ) {
-								continue;
-							}
-							var filename = Path.Combine ( directory.FullName, iks.PathSafe ( ) );
-							Console.WriteLine ( "Saving: {0}", Path.GetFileNameWithoutExtension ( filename ) );
-							var hash = Hash ( img );
-							if ( hashes.ContainsValue ( hash ) && iconKey != DEFAULT ) {
-								if ( dupes.ContainsKey ( hash ) ) {
-									dupes[hash].Add ( Path.GetFileNameWithoutExtension ( iks ) );
-								} else {
-									dupes.Add ( hash, new List<string> { Path.GetFileNameWithoutExtension ( iks ) } );
-								}
-							}
-
-							hashes.Add ( iks, hash );
-
-							img.Save ( filename, ImageFormat.Png );
-						}
-					}
-				}
-
+				ProcessFileExtension ( icon.Key );
 			}
-			Console.WriteLine ( "Remove Dupes..." );
-			foreach ( var f in dupes.SelectMany ( dupe => dupe.Value ) ) {
-				hashes.Remove ( String.Format ( "{0}.png", f ) );
-				// create the map
-				File.Delete ( Path.Combine ( directory.FullName, String.Format ( "{0}.png", f ) ) );
-			}
+
+			RemoveDuplicates ( );
 
 			var classList = new Dictionary<String, String> ( );
-			foreach ( var dupe in dupes ) {
-				var hashKey = hashes.First ( h => h.Value == dupe.Key ).Key;
+			foreach ( var dupe in Dupes ) {
+				var hashKey = Hashes.First ( h => h.Value == dupe.Key ).Key;
 				var classKey = Path.GetFileNameWithoutExtension ( hashKey );
-				hashes.Remove ( hashKey );
+				Hashes.Remove ( hashKey );
 				var classes = String.Format ( ".{2}-{0}, .{2}-{1}", classKey, String.Join ( String.Format ( ", .{0}-", FYLES ), dupe.Value ), FYLES );
 				Console.WriteLine ( "Adding Group: {0}", hashKey );
 				classList.Add ( classKey, classes );
 			}
 
-			var remainingFiles = directory.GetFiles ( ).OrderBy
+			var remainingFiles = OutputDirectory.GetFiles ( ).OrderBy
 				( f => {
 					var fn = Path.GetFileNameWithoutExtension ( f.Name );
 					return fn.Substring ( 0, fn.LastIndexOf ( "-" ) );
@@ -102,12 +70,10 @@ namespace Fyles {
 			const int perRow = 6;
 			const int padding = 5;
 
-			var rowCount = (int)Math.Round ( (double)remainingFiles.Count ( ) / ( (double)widths.Count * perRow ) );
-			var totalPadding = widths.Count * padding;
-
-
-			var maxWidth = ( widths.Sum ( ) + totalPadding ) * perRow;
-			var rowHeight = widths.Max ( ) + padding;
+			var rowCount = (int)Math.Round ( (double)remainingFiles.Count ( ) / ( (double)Widths.Count * perRow ) );
+			var totalPadding = Widths.Count * padding;
+			var maxWidth = ( Widths.Sum ( ) + totalPadding ) * perRow;
+			var rowHeight = Widths.Max ( ) + padding;
 			var maxHeight = rowHeight * rowCount;
 
 			var defaultPositions = new Dictionary<int, Point> ( );
@@ -116,7 +82,7 @@ namespace Fyles {
 
 				using ( var graphics = Graphics.FromImage ( sprite ) ) {
 					using (
-						var css = new FileStream ( Path.Combine ( directory.Parent.FullName, FYLES + ".css" ), FileMode.Create,
+						var css = new FileStream ( Path.Combine ( OutputDirectory.Parent.FullName, FYLES + ".css" ), FileMode.Create,
 																			 FileAccess.Write ) ) {
 						using ( var writer = new StreamWriter ( css ) ) {
 
@@ -148,7 +114,7 @@ namespace Fyles {
 																		-location.Y, location.Y == 0 ? "" : "px", size );
 								mainLines.Append ( "}" );
 								location.Offset ( size + padding, 0 );
-								if ( location.X >= maxWidth ) {
+								if ( (location.X+size + padding) >= maxWidth ) {
 									location.X = 0;
 									location.Offset ( 0, rowHeight );
 								}
@@ -156,23 +122,70 @@ namespace Fyles {
 								Console.WriteLine ( "Location: {0}", location );
 							}
 
-							foreach ( var w in widths ) {
+							foreach ( var w in Widths ) {
 								var pos = defaultPositions[w];
-								line.AppendFormat ( ".{0}-{1}{{height: {1}px; width: {1}px; background-position: {2}px {3}px;}}{4}", FYLES, w,
-																		-pos.X, -pos.Y, Environment.NewLine );
+								line.AppendFormat ( ".{0}-{1}{{height: {1}px; width: {1}px; background-position: {2}{3} {4}{5};}}{6}", FYLES, w,
+									-pos.X, pos.X == 0 ? "" : "px", -pos.Y, pos.Y == 0 ? "" : "px", Environment.NewLine );
 							}
 
 							writer.WriteLine ( line );
-							writer.Write(mainLines);
+							writer.Write ( mainLines );
 
 						}
 					}
 				}
-				sprite.Save ( Path.Combine ( directory.Parent.FullName, FYLES + ".png" ), ImageFormat.Png );
+				sprite.Save ( Path.Combine ( OutputDirectory.Parent.FullName, FYLES + ".png" ), ImageFormat.Png );
 			}
 
 			Console.WriteLine ( "Press {ENTER} to exit..." );
 			Console.ReadLine ( );
+		}
+
+		static void RemoveDuplicates ( ) {
+			Console.WriteLine ( "Remove Dupes..." );
+			foreach ( var f in Dupes.SelectMany ( dupe => dupe.Value ) ) {
+				Hashes.Remove ( String.Format ( "{0}.png", f ) );
+				// create the map
+				File.Delete ( Path.Combine ( OutputDirectory.FullName, String.Format ( "{0}.png", f ) ) );
+			}
+		}
+
+		static void ProcessFileExtension ( String extension ) {
+			foreach ( var esize in Enum.GetValues ( typeof ( IconReader.IconSize ) ) ) {
+				var shfi = new Fyles.WinApi.Shell32.SHFILEINFO ( );
+
+				using ( var ico = IconReader.ExtractIconFromFileEx ( FYLES + extension, (IconReader.IconSize)esize, ref shfi ) ) {
+					if ( ico == null || ico.Width > MaxSize ) {
+						continue;
+					}
+
+					var size = String.Format ( "{0}", ico.Height, ico.Width );
+					if ( !Widths.Contains ( ico.Width ) ) {
+						Widths.Add ( ico.Width );
+					}
+					using ( var img = ico.ToBitmap ( ) ) {
+						var iconKey = extension.Substring ( 1 );
+						var iks = String.Format ( "{0}-{1}.png", iconKey, size );
+						if ( Hashes.ContainsKey ( iks ) ) {
+							continue;
+						}
+						var filename = Path.Combine ( OutputDirectory.FullName, iks.PathSafe ( ) );
+						Console.WriteLine ( "Saving: {0}", Path.GetFileNameWithoutExtension ( filename ) );
+						var hash = Hash ( img );
+						if ( Hashes.ContainsValue ( hash ) && iconKey != DEFAULT ) {
+							if ( Dupes.ContainsKey ( hash ) ) {
+								Dupes[hash].Add ( Path.GetFileNameWithoutExtension ( iks ) );
+							} else {
+								Dupes.Add ( hash, new List<string> { Path.GetFileNameWithoutExtension ( iks ) } );
+							}
+						}
+
+						Hashes.Add ( iks, hash );
+
+						img.Save ( filename, ImageFormat.Png );
+					}
+				}
+			}
 		}
 
 		static String Hash ( Image img ) {
